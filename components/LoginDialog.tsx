d3e031e0ead176e3c8b32dc53f6081dce072a802
farish-jamal <farishjamal98@gmail.com>
2024-10-20 02:40:28 +0530

    Added Logout and UI Fixes

--- components/LoginDialog.tsx
+++ components/LoginDialog.tsx
@@ -4,37 +4,14 @@ import {
  Dialog,
  DialogContent,
  DialogHeader,
- DialogTitle,
  DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { Loader2 } from "lucide-react";
-import { useState, ChangeEvent } from "react";
-import { toast } from "sonner";
+import { signIn } from "next-auth/react";
+import { GithubIcon, SlackIcon } from "lucide-react";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 const LoginDialog: React.FC = () => {
- const [loading, setLoading] = useState<boolean>(false);
- const [email, setEmail] = useState<string>("");
- const [password, setPassword] = useState<string>("");
-
- const handleSubmit = () => {
-  setLoading(true);
-  console.log(email);
-  if (email.length == 0 || password.length == 0) {
-   toast("Please fill all fields.");
-   setLoading(false);
-   return;
-  }
- };
-
- const handleEmailChange = (e: ChangeEvent<HTMLInputElement>): void => {
-  setEmail(e.target.value);
- };
-
- const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
-  setPassword(e.target.value);
- };
 
  return (
   <Dialog>
@@ -42,28 +19,12 @@ const LoginDialog: React.FC = () => {
     Login
    </DialogTrigger>
    <DialogContent>
-    <DialogHeader className="space-y-5">
-     <DialogTitle>Welcome Back!</DialogTitle>
-     <Input
-      value={email}
-      onChange={handleEmailChange}
-      type="email"
-      placeholder="Email"
-     />
-     <Input
-      value={password}
-      onChange={handlePasswordChange}
-      type="password"
-      placeholder="Password"
-     />
-     {!loading ? (
-      <Button onClick={handleSubmit}>Login</Button>
-     ) : (
-      <Button disabled>
-       <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-       Please wait
-      </Button>
-     )}
+   <DialogTitle>Welcome back</DialogTitle>
+    <DialogHeader className="space-y-3 mt-5">
+      <Button onClick={() => signIn("github")}>Sign In With Github <GithubIcon /></Button>
+     <p className="text-center">OR</p>
+     <Button onClick={() => signIn("github")}>Sign In With Google <SlackIcon /></Button>
+       
     </DialogHeader>
    </DialogContent>
   </Dialog>
---
+++ components/LogoutDropDown.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { signOut, useSession } from "next-auth/react";
+import { LogOutIcon } from "lucide-react";
+
+const LogoutDropDown = () => {
+  const { data: session } = useSession();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={session?.user?.image} />
+          <AvatarFallback>U</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
+          {" "}
+          <LogOutIcon /> LogOut
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default LogoutDropDown;
